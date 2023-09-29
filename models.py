d85b4e555bd36c369630764bcec1cc9473684596
Tharuka Kasthuri Arachchige <tharuka.ckasthuri@gmail.com>
2023-09-29 18:02:04 +0200

    optimized isolated training

--- models.py
+++ models.py
@@ -1,37 +1,44 @@
 import torch
 
+
 class ShallowNN(torch.nn.Module):
     """
     Shallow Nueral Network model with three layers.
-    
+
     Parameters:
     ------------
     feats: int; number of features
     """
-    def __init__(self,feats):
+
+    def __init__(self, feats):
         super(ShallowNN, self).__init__()
-        self.layer_1 = torch.nn.Linear(feats,64)
+        self.layer_1 = torch.nn.Linear(feats, 64)
         self.relu_1 = torch.nn.ReLU()
-        self.layer_2 = torch.nn.Linear(64,32)
+        self.layer_2 = torch.nn.Linear(64, 32)
         self.relu_2 = torch.nn.ReLU()
         self.layer_3 = torch.nn.Linear(32, 1)
-        self.track_layers = {"layer_1" : self.layer_1, "layer_2": self.layer_2, "layer_3": self.layer_3}
-        
-        
+        self.track_layers = {
+            "layer_1": self.layer_1,
+            "layer_2": self.layer_2,
+            "layer_3": self.layer_3,
+        }
+
     def forward(self, inputs):
         """
         Forward pass.
-        
+
         Parameters:
         ------------
         inputs: torch.tensor object; input data
-        
+
         Returns:
         ------------
         x: torch.tensor object; output data
         """
         x = self.relu_1(self.layer_1(inputs))
         x = self.relu_2(self.layer_2(x))
-        x = self.layer_3(x)        
+        x = self.layer_3(x)
         return x
-    
+
+
+### When the feats = 197,
--- preprocess.py
+++ preprocess.py
@@ -10,7 +10,8 @@ from sklearn.model_selection import train_test_split
 
 from utils import CustomDataSet
 
-def load_file(file_path)->pd.DataFrame:
+
+def load_file(file_path) -> pd.DataFrame:
     """
     Loads the pickle file into a dataframe.
 
@@ -22,15 +23,21 @@ def load_file(file_path)->pd.DataFrame:
     ------------
     """
     tmp = pd.read_pickle(file_path)
-    df = pd.merge(left=tmp["dataset"]["X"], right=tmp["dataset"]
-                  ["Y"], how="left", left_index=True, right_index=True)
+    df = pd.merge(
+        left=tmp["dataset"]["X"],
+        right=tmp["dataset"]["Y"],
+        how="left",
+        left_index=True,
+        right_index=True,
+    )
     df.drop(columns=["TimeStamp", "WritesAvg"], inplace=True)
     df.rename(columns={"ReadsAvg": "label"}, inplace=True)
     df.reset_index(inplace=True, drop=True)
 
     return df
 
-def build_dataset(dataframe, client_id)->None:
+
+def build_dataset(dataframe, client_id) -> None:
     """
     Split the dataframe into train and test, saving as a pytorch dataset.
 
@@ -55,22 +62,27 @@ def build_dataset(dataframe, client_id)->None:
     scaler_y = StandardScaler()
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.33, random_state=42)
+        X, y, test_size=0.20, random_state=42
+    )
 
-    X_train = pd.DataFrame(scaler_x.fit_transform(
-        X_train.values), index=X_train.index, columns=X_train.columns)
-    
-    X_test = pd.DataFrame(scaler_x.transform(
-        X_test.values), index=X_test.index, columns=X_test.columns)
-    
+    X_train = pd.DataFrame(
+        scaler_x.fit_transform(X_train.values),
+        index=X_train.index,
+        columns=X_train.columns,
+    )
+
+    X_test = pd.DataFrame(
+        scaler_x.transform(X_test.values), index=X_test.index, columns=X_test.columns
+    )
+
     y_train = scaler_y.fit_transform(y_train.reshape(-1, 1))
     y_test = scaler_y.transform(y_test.reshape(-1, 1))
 
     train_dataset = CustomDataSet(X_train, y_train)
     test_dataset = CustomDataSet(X_test, y_test)
 
-    torch.save(train_dataset, "./trainpt/" +str(client_id) +".pt")
-    torch.save(test_dataset, "./testpt/" +str(client_id) +".pt")
+    torch.save(train_dataset, "./trainpt/" + str(client_id) + ".pt")
+    torch.save(test_dataset, "./testpt/" + str(client_id) + ".pt")
 
 
 def main():
@@ -79,8 +91,9 @@ def main():
     ids = [file.split(".")[0] for file in files]
     files_path = [os.path.join(data_path, file) for file in files]
 
-    for id, pickle_file in zip(ids,files_path):
+    for id, pickle_file in zip(ids, files_path):
         build_dataset(load_file(pickle_file), id)
+
 
 if __name__ == "__main__":
     main()
